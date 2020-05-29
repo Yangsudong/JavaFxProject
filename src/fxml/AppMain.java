ee7f76ad00c315d7ecf8e0fd8f49d15d6b8c6238
admin <tnehdtlrl@naver.com>
2020-05-29 12:53:52 +0900

    0529

--- src/fxml/AppMain.java
+++ src/fxml/AppMain.java
@@ -5,13 +5,16 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.Observable;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class AppMain extends Application {
 	@Override
@@ -22,20 +25,43 @@ public class AppMain extends Application {
 		
 		TextField textField = new TextField();		// TextField 컨트롤 생성
 		textField.setPrefWidth(200);				// TextField의 폭 설정
+		textField.setPrefHeight(100);
 		
 		Button button = new Button();				// Button 컨트롤 생성
 		button.setText("확인");						// Button 글자 설정
-		button.setOnAction(event -> Platform.exit());
+		button.setPrefSize(100, 100);
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				textField.setText("확인을 눌렀습니다");
+			}
+		});
 		
+		Button button1 = new Button();				// Button 컨트롤 생성
+		button1.setText("취소");						// Button 글자 설정
+		button1.setPrefSize(100, 100);
+		button1.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				textField.setText(null);
+			}
+		});
 		ObservableList list = hbox.getChildren();	// Hbox의 ObservableList 얻기
 		list.add(textField);						// TextField 컨트롤 배치
 		list.add(button);							// Button의 컨트롤 배치
+		list.add(button1);
 		
 		Scene scene = new Scene(hbox);				// 장면생성
 		
-		primaryStage.setTitle("AppMain");			// 윈도우 창 제목설정
+		primaryStage.setTitle("레이아웃");			// 윈도우 창 제목설정
 		primaryStage.setScene(scene);				// 윈도우 창에 장면 설정
 		primaryStage.show();						// 윈도우 창 보여주기
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent event) {
+				System.out.println("닫기 버튼 클릭.");
+			}
+		});
 	}
 	
 	public static void main(String[] args) {
